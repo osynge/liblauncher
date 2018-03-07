2ced132edd6143e05fb0f996bbd73df36430f5da
Owen Synge <owen.synge@jaysnest.de>
2018-03-07 21:55:10 +0100

    Centralise closing file descriptors
    Added new functions:
    
    close_file_descriptor_read
    close_file_descriptor_write
    close_file_descriptor_child
    close_file_descriptor_pairent
    
    And called them in replacement of posix_close
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/redirect.rs
+++ src/redirect.rs
@@ -9,13 +9,12 @@ use const_api;
 
 #[derive(Debug)]
 pub struct LauncherStructPipe {
-    pub file_descriptor_read: Option<u32>, // FIFO read
-    pub file_descriptor_write: Option<u32>, // FIFO write
-    pub file_descriptor_child: Option<u32>, // Child ID wanted
+    pub file_descriptor_read: Option<u32>,    // FIFO read
+    pub file_descriptor_write: Option<u32>,   // FIFO write
+    pub file_descriptor_child: Option<u32>,   // Child ID wanted
     pub file_descriptor_pairent: Option<u32>, // Pairent ID to mirror
     pub redirect: Option<const_api::RedirectType>,
 }
-
 
 #[derive(Debug)]
 pub enum LauncherStructPipeError {
@@ -26,9 +25,7 @@ pub enum LauncherStructPipeError {
     RedirectMirror,
 }
 
-
 fn posix_close(file_num: c_int) -> Result<(), const_api::LauncherError> {
-
     unsafe {
         let rc: c_int;
         rc = close(file_num);
@@ -52,7 +49,6 @@ fn mkpipe() -> Result<(u32, u32), const_api::LauncherError> {
     let file_descriptor_write = pipeparam[1] as u32;
     Ok((file_descriptor_read, file_descriptor_write))
 }
-
 
 fn posix_dup(oldfd: c_int) -> Result<c_int, const_api::LauncherError> {
     let dup2_rc: c_int;
@@ -96,6 +92,58 @@ impl LauncherStructPipe {
         return Ok(());
     }
 
+    fn close_file_descriptor_read(&mut self) -> Result<(), LauncherStructPipeError> {
+        let filedes_w = self.file_descriptor_read.unwrap() as c_int;
+        match posix_close(filedes_w) {
+            Ok(()) => {
+                self.file_descriptor_read = None;
+            }
+            Err(_) => {
+                return Err(LauncherStructPipeError::Unknown);
+            }
+        }
+        Ok(())
+    }
+
+    fn close_file_descriptor_write(&mut self) -> Result<(), LauncherStructPipeError> {
+        let filedes_w = self.file_descriptor_write.unwrap() as c_int;
+        match posix_close(filedes_w) {
+            Ok(()) => {
+                self.file_descriptor_write = None;
+            }
+            Err(_) => {
+                return Err(LauncherStructPipeError::Unknown);
+            }
+        }
+        Ok(())
+    }
+
+    fn close_file_descriptor_child(&mut self) -> Result<(), LauncherStructPipeError> {
+        let filedes_w = self.file_descriptor_child.unwrap() as c_int;
+        match posix_close(filedes_w) {
+            Ok(()) => {
+                self.file_descriptor_child = None;
+            }
+            Err(_) => {
+                return Err(LauncherStructPipeError::Unknown);
+            }
+        }
+        Ok(())
+    }
+
+    fn close_file_descriptor_pairent(&mut self) -> Result<(), LauncherStructPipeError> {
+        let filedes_w = self.file_descriptor_pairent.unwrap() as c_int;
+        match posix_close(filedes_w) {
+            Ok(()) => {
+                self.file_descriptor_pairent = None;
+            }
+            Err(_) => {
+                return Err(LauncherStructPipeError::Unknown);
+            }
+        }
+        Ok(())
+    }
+
     fn prep_launch_mirror(&mut self) -> Result<(), LauncherStructPipeError> {
         match self.file_descriptor_write {
             Some(fd_int) => {
@@ -110,7 +158,6 @@ impl LauncherStructPipe {
             None => {
                 return Err(LauncherStructPipeError::Unknown);
             }
-
         }
         let filedes_w: c_int;
         match self.file_descriptor_pairent {
@@ -118,7 +165,6 @@ impl LauncherStructPipe {
             None => {
                 filedes_w = filedes_c;
             }
-
         }
         if filedes_w == filedes_c {
             ()
@@ -132,7 +178,6 @@ impl LauncherStructPipe {
         }
         Ok(())
     }
-
 
     pub fn prep_launch(&mut self) -> Result<(), LauncherStructPipeError> {
         let action: const_api::RedirectType;
@@ -159,17 +204,16 @@ impl LauncherStructPipe {
         }
     }
 
-
     fn post_launch_child_close(&mut self) -> Result<(), LauncherStructPipeError> {
-        let filedes_w = self.file_descriptor_pairent.unwrap() as c_int;
-        match posix_close(filedes_w) {
+        let filedes_w = self.close_file_descriptor_pairent();
+        match filedes_w {
             Ok(()) => {}
             Err(_) => {
                 return Err(LauncherStructPipeError::Unknown);
             }
         }
         self.file_descriptor_pairent = self.file_descriptor_write;
-        self.file_descriptor_read = None;
+        self.file_descriptor_write = None;
         Ok(())
     }
 
@@ -178,17 +222,13 @@ impl LauncherStructPipe {
         let filedes_r = self.file_descriptor_child.unwrap() as c_int;
         let foo = posix_dup2(filedes_w, filedes_r);
         match foo {
-            Ok(()) =>
-            {
-            }
+            Ok(()) => {}
             Err(_) => {
                 return Err(LauncherStructPipeError::Unknown);
             }
         }
-        match posix_close(filedes_w) {
-            Ok(()) => {
-                self.file_descriptor_write = None;
-            }
+        match self.close_file_descriptor_write() {
+            Ok(()) => {}
             Err(_) => {
                 return Err(LauncherStructPipeError::Unknown);
             }
@@ -198,25 +238,23 @@ impl LauncherStructPipe {
 
     fn post_launch_child_redirect_write(&mut self) -> Result<(), LauncherStructPipeError> {
         let filedes_w = self.file_descriptor_read.unwrap() as c_int;
-        let filedes_r = self.file_descriptor_child.unwrap() as c_int;
-        let foo = posix_dup2(filedes_w, filedes_r);
+        let filedes_c = self.file_descriptor_child.unwrap() as c_int;
+        let foo = posix_dup2(filedes_w, filedes_c);
         match foo {
             Ok(()) => {}
             Err(_) => {
                 return Err(LauncherStructPipeError::Unknown);
             }
         }
-        match posix_close(filedes_r) {
-            Ok(()) => {
-                self.file_descriptor_read = None;
-            }
+        match self.close_file_descriptor_read() {
+            Ok(()) => {}
             Err(_) => {
                 return Err(LauncherStructPipeError::Unknown);
             }
         }
         self.post_launch_child_close()
-
     }
+
     fn post_launch_child_mirror(&mut self) -> Result<(), LauncherStructPipeError> {
         let filedes_c: c_int;
         match self.file_descriptor_child {
@@ -236,33 +274,31 @@ impl LauncherStructPipe {
             ()
         }
         let filedes_j = self.file_descriptor_write.unwrap() as c_int;
-
-        let posix_dup_rc = posix_dup2(filedes_j, filedes_c);
-        match posix_dup_rc {
-            Ok(()) => {
-            }
+        match self.close_file_descriptor_write() {
+            Ok(()) => {}
             Err(_) => {
                 return Err(LauncherStructPipeError::Unknown);
             }
         }
-        let close_ok = posix_close(filedes_j);
-        match close_ok {
-            Ok(()) => {
-                self.file_descriptor_write = None;
+        let posix_dup_rc = posix_dup2(filedes_j, filedes_c);
+        match posix_dup_rc {
+            Ok(()) => {}
+            Err(_) => {
+                return Err(LauncherStructPipeError::Unknown);
             }
+        }
+        match self.close_file_descriptor_write() {
+            Ok(()) => {}
             Err(_) => {
                 return Err(LauncherStructPipeError::Unknown);
             }
         }
         Ok(())
     }
+
     fn post_launch_child_ignore(&mut self) -> Result<(), LauncherStructPipeError> {
-        let filedes_r = self.file_descriptor_child.unwrap() as c_int;
-        let close_ok = posix_close(filedes_r);
-        match close_ok {
-            Ok(()) =>
-            {
-            }
+        match self.close_file_descriptor_child() {
+            Ok(()) => {}
             Err(_) => {
                 return Err(LauncherStructPipeError::Unknown);
             }
@@ -271,7 +307,6 @@ impl LauncherStructPipe {
         self.file_descriptor_read = None;
         Ok(())
     }
-
 
     pub(crate) fn post_launch_child(&mut self) -> Result<(), LauncherStructPipeError> {
         let action: const_api::RedirectType;
@@ -296,10 +331,8 @@ impl LauncherStructPipe {
         }
     }
 
-
     fn post_launch_pairent_redirect_read(&mut self) -> Result<(), LauncherStructPipeError> {
-        let filedes_w = self.file_descriptor_write.unwrap() as c_int;
-        match posix_close(filedes_w) {
+        match self.close_file_descriptor_write() {
             Ok(()) => {
                 self.file_descriptor_write = None;
             }
@@ -313,8 +346,7 @@ impl LauncherStructPipe {
     }
 
     fn post_launch_pairent_redirect_write(&mut self) -> Result<(), LauncherStructPipeError> {
-        let filedes_w = self.file_descriptor_read.unwrap() as c_int;
-        match posix_close(filedes_w) {
+        match self.close_file_descriptor_read() {
             Ok(()) => {
                 self.file_descriptor_read = None;
             }
@@ -332,19 +364,10 @@ impl LauncherStructPipe {
     }
 
     fn post_launch_pairent_redirect_mirror(&mut self) -> Result<(), LauncherStructPipeError> {
-        match self.file_descriptor_write {
-            Some(n) => {
-                match posix_close(n as i32) {
-                    Ok(()) => {}
-                    Err(_) => {
-                        return Err(LauncherStructPipeError::Unknown);
-                    }
-                }
-                self.file_descriptor_write = None;
-            }
-            None => {}
+        match self.close_file_descriptor_write() {
+            Ok(()) => Ok(()),
+            Err(_) => Err(LauncherStructPipeError::Unknown),
         }
-        Ok(())
     }
 
     pub(crate) fn post_launch_pairent(&mut self) -> Result<(), LauncherStructPipeError> {
@@ -361,7 +384,6 @@ impl LauncherStructPipe {
                 }
             }
         }
-
 
         match action {
             const_api::RedirectType::RedirectRead => {
