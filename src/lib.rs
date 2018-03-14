f59ff389450b9477a71a693c920bce8a3ab3c805
Owen Synge <owen.synge@jaysnest.de>
2018-03-14 23:55:17 +0100

    Delete legacy API
    We dont want to maintain thsi API as new API is better.
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,13 @@
 extern crate libc;
 
 mod wrap_posix;
-mod redirect;
 mod const_api;
-mod feaer;
 mod ceaer;
-mod redirect_container;
 mod redirect_map;
 mod redirect_factory;
 mod redirect_map_factory;
 mod redirect_process;
 
 pub use const_api::RedirectType;
-pub use feaer::Launcher;
 pub use ceaer::Ceaer;
 pub use ceaer::Process;
