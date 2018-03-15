4db28700909993488b293d58bb21a5667b3999e5
Owen Synge <owen.synge@jaysnest.de>
2018-03-15 03:27:48 +0100

    add mod process
    And export process::Process
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate libc;
 mod wrap_posix;
 mod const_api;
 mod ceaer;
+mod process;
 mod redirect_map;
 mod redirect_factory;
 mod redirect_map_factory;
@@ -10,4 +11,4 @@ mod redirect_process;
 
 pub use const_api::RedirectType;
 pub use ceaer::Ceaer;
-pub use ceaer::Process;
+pub use process::Process;
