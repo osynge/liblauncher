c866eb26964c23b1fa31954c1fdf6cb38e3577da
Owen Synge <owen.synge@jaysnest.de>
2018-03-15 02:54:41 +0100

    Remove unused code
    This code was left over from previosu cut n paste
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/redirect_map.rs
+++ src/redirect_map.rs
@@ -2,56 +2,11 @@ use const_api;
 use std::collections::HashMap;
 
 use redirect_factory;
-
 use redirect_process;
-
-#[derive(Debug)]
-pub struct RedirectMapFactory {
-    pub(crate) redirect: HashMap<u32, redirect_factory::RedirectFactory>,
-}
 
 #[derive(Debug)]
 pub struct RedirectMapContainer {
     pub(crate) redirect: HashMap<u32, redirect_process::RedirectProcess>,
-}
-
-impl RedirectMapFactory {
-    pub fn new() -> Result<RedirectMapFactory, String> {
-        Ok(RedirectMapFactory {
-            redirect: HashMap::new(),
-        })
-    }
-    pub(crate) fn redirect_set(
-        &mut self,
-        child_fd: u32,
-        pairent_fd: Option<u32>,
-        redirect_type: Option<const_api::RedirectType>,
-    ) -> const_api::LaunchResult {
-        let mut bill = redirect_factory::RedirectFactory::new().unwrap();
-        bill.redirect = redirect_type;
-        bill.file_descriptor_child = Some(child_fd);
-        bill.file_descriptor_pairent = pairent_fd;
-        self.redirect.insert(child_fd, bill);
-        Ok(())
-    }
-
-    pub(crate) fn generate_container(&mut self) -> Result<RedirectMapContainer, String> {
-        let mut output = RedirectMapContainer {
-            redirect: HashMap::new(),
-        };
-        for (mut sd, mut fd) in self.redirect.iter_mut() {
-            let fd_mapping: u32 = *sd;
-            match fd.generate_process() {
-                Ok(factory) => {
-                    output.redirect.insert(fd_mapping, factory);
-                }
-                Err(_) => {
-                    return Err(String::new());
-                }
-            };
-        }
-        Ok(output)
-    }
 }
 
 impl RedirectMapContainer {
